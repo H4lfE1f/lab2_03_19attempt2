19ae5dce152bcbe579467ecb447add6f545e126f
lamossus <zhora.polyakov95@mail.ru>
2020-09-14 20:45:32 +0300

    Initial commit

---
+++ src/ru/lab/mirea2/author.java
@@ -0,0 +1,29 @@
+package ru.lab.mirea2;
+import java.lang.*;
+
+public class author
+{
+    private String name;
+    private String email;
+    private char gender;
+
+    public author(String name, String email, char gender){
+        this.name=name;
+        this.email=email;
+        this.gender=gender;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getEmail(){
+
+        return email;
+    }
+    public char getGender(){
+        return gender;
+    }
+    public author(){
+        gender="";
+        gender=
+    }
+}
