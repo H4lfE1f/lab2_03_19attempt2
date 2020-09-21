b314aa966aa17b041d61f57c3ab46292ba50a81f
Nate <nigllemmesmash@gmail.com>
2020-09-21 19:44:08 +0300

    //

--- src/ru/lab/mirea2/testauthor.java
+++ src/ru/lab/mirea2/testauthor.java
@@ -4,5 +4,6 @@ public class testauthor {
     public static void main(String[] args){
         author b1 = new author("Pushkin","pushkin@yandex.ru",'M');
         System.out.println(b1);
+        //
     }
 }
